df15a10cb3a5bfa09f566e1a0f722423caf8c3a0
Dawson Rudolph <dawsonrudolph03@gmail.com>
2024-11-26 08:44:57 -0600

    Login Service

---
+++ Services/Login.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = json.loads(request.data)
+    if user['username'] == 'TopicsInSE' and user['password'] == 'CS480':
+        return jsonify({'message': 'Correct username & password. You are now logged in.'}), 200
+    else:
+        return jsonify({'message': 'Incorrect username or password. Please try again.'}), 400
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    return jsonify({'messsage': 'You are now logged out.'}), 200
+
+if __name__ == '__main__':
+   app.run(port=5000)
