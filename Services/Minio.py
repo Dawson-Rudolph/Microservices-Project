8200e6a3ad2f437840c5636ddd15d341ff8c80c4
Dawson Rudolph <dawsonrudolph03@gmail.com>
2024-11-27 16:50:00 -0600

    CRUD + S3 application

---
+++ Services/Minio.py
@@ -0,0 +1,61 @@
+from minio import Minio
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+# Run this function in terminal to start your Minio server:
+# docker run -p 9000:9000 -p 9001:9001 --name minio -d quay.io/minio/minio server /data --console-address ":9001"
+
+# create function
+@app.route('/upload', methods=['PUT'])
+def upload():
+    response = json.loads(request.data)
+    client = Minio(response['url'], access_key=response['accessKey'], secret_key=response['secretKey'], secure=False)
+    
+    if client.bucket_exists(response['bucketName']):
+        client.fput_object(response['bucketName'], response['filename'], response['filepath'])
+        return jsonify({'message': 'File successfully uploaded'}), 200
+    else:
+        return jsonify({'message': 'Bucket does not exist'}), 400
+    
+# read function
+@app.route('/list', methods=['GET'])
+def list():
+    response = json.loads(request.data)
+    client = Minio(response['url'], access_key=response['accessKey'], secret_key=response['secretKey'], secure=False)
+
+    if client.bucket_exists(response['bucketName']):
+        objects = client.list_objects(response['bucketName'], recursive=True)
+        object_names = [obj.object_name for obj in objects]
+        return jsonify({'objectNames': object_names}), 200
+    else:
+        return jsonify({'message': 'Bucket does not exist'}), 400
+
+# update function
+@app.route('/update', methods=['PATCH'])
+def update():
+    response = json.loads(request.data)
+    client = Minio(response['url'], access_key=response['accessKey'], secret_key=response['secretKey'], secure=False)
+
+    if client.bucket_exists(response['bucketName']):
+        client.remove_object(response['bucketName'], response['filename'])
+        client.fput_object(response['bucketName'], response['filename'], response['filepath'])
+        return jsonify({'message': 'File successfully updated'}), 200
+    else:
+        return jsonify({'message': 'Bucket does not exist'}), 400
+
+# delete function
+@app.route('/delete', methods=['DELETE'])
+def delete():
+    response = json.loads(request.data)
+    client = Minio(response['url'], access_key=response['accessKey'], secret_key=response['secretKey'], secure=False)
+
+    if client.bucket_exists(response['bucketName']):
+        client.remove_object(response['bucketName'], response['filename'])
+        return jsonify({'message': 'File successfully deleted'}), 200
+    else:
+        return jsonify({'message': 'Bucket does not exist'}), 400
+
+if __name__ == '__main__':
+   app.run(port=5001)
